a77f9f51bd5c3f99c2b87c109867580cb5708bc9
Agassis Rodrigues <61802302+agassis-1@users.noreply.github.com>
2020-03-11 16:54:36 -0300

    Update Incompleto.sh

--- Leonid/Incompleto.sh
+++ Leonid/Incompleto.sh
@@ -2,9 +2,13 @@
 
 ss=$1
 
+#dominio
 rm -rf "/etc/bind/db.$ss.com.br"
+
+#site
 rm -rf "/etc/apache2/sites-available/$ss"
 rm -rf "/var/www/$ss"
 
+#ftp
 deluser $ss -shell /bin/false -home /home/$ss
 deluser $ss userftp
